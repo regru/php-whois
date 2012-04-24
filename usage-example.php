d3a84d4292f4bbb7078e92522572ab950261d985
git <git@git.reg.ru>
2012-04-25 02:19:36 +0400

    Init

---
+++ usage-example.php
@@ -0,0 +1,18 @@
+<?
+
+include('whois.class.php');
+
+$domain_name = 'reg.ru';
+
+$domain = new whois( $domain_name );
+$whois_answer = $domain->info();
+
+echo $whois_answer;
+
+if ($domain->is_available()) {
+    echo "Domain is available\n";
+} else {
+    echo "Domain is registered\n";
+}
+
+?>
