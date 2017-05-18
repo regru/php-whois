1c8166347803df433001bd13acee45b96a610026
askello <oleksii.korpan@gmail.com>
2017-05-18 18:13:00 +0300

    fixed regexp for working with "seo.gruz0.ru"

--- src/Phois/Whois/Whois.php
+++ src/Phois/Whois/Whois.php
@@ -20,7 +20,7 @@ class Whois
         $this->domain = $domain;
         // check $domain syntax and split full domain name on subdomain and TLDs
         if (
-            preg_match('/^([\p{L}\d\-]+)\.((?:[\p{L}\-]+\.?)+)$/ui', $this->domain, $matches)
+            preg_match('/^([\p{L}\d\-]+)\.((?:[\p{L}\d\-]+\.?)+)$/ui', $this->domain, $matches)
             || preg_match('/^(xn\-\-[\p{L}\d\-]+)\.(xn\-\-(?:[a-z\d-]+\.?1?)+)$/ui', $this->domain, $matches)
         ) {
             $this->subDomain = $matches[1];
